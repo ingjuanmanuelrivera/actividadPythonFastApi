ba1b44dce805500a38f13768476621be3100ee76
pixelbi <47459781+pixelbi18@users.noreply.github.com>
2025-10-15 20:45:44 -0500

    Protección fuentes

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class Item(BaseModel):
+    id: int
+    name: str
+    description: Optional[str] = None
+    done: bool = False
+
+
+class ItemCreate(BaseModel):
+    name: str
+    description: Optional[str] = None
